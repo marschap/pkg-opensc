3543b38cfada935c0a727e669def8a45ba05f6a2
Eric Dorland <eric@debian.org>
2004-10-08 07:12:29 +0000

    This commit was generated by cvs2svn to compensate for changes in r44941, which included commits to RCS files with non-trunk default branches.

--- src/sslengines/engine_pkcs11.c
+++ src/sslengines/engine_pkcs11.c
@@ -36,13 +36,27 @@
 
 PKCS11_CTX *ctx;
 char *pin = NULL;
-int quiet = 1;
+int verbose = 0;
 
-const char *module = PKCS11_DEFAULT_MODULE_NAME;
+char *module = PKCS11_DEFAULT_MODULE_NAME;
+int default_module = 1;
 
 int set_module(const char *modulename)
 {
-	module = modulename;
+	module = strdup (modulename);
+	default_module = 0;
+	return 1;
+}
+
+int set_pin(const char *_pin)
+{
+	pin = strdup(_pin);
+	return 1;
+}
+
+int inc_verbose()
+{
+	verbose++;
 	return 1;
 }
 
@@ -51,14 +65,16 @@ char *get_pin(UI_METHOD * ui_method, char *sc_pin, int maxlen)
 	UI *ui;
 
 	ui = UI_new();
-	UI_set_method(ui, ui_method);
+	if (ui_method != NULL)
+	    UI_set_method(ui, ui_method);
 	if (!UI_add_input_string(ui, "SmartCard PIN: ", 0, sc_pin, 1, maxlen)) {
 		fprintf(stderr, "UI_add_input_string failed\n");
 		UI_free(ui);
 		return NULL;
 	}
-	if (!UI_process(ui)) {
+	if (UI_process(ui)) {
 		fprintf(stderr, "UI_process failed\n");
+		UI_free(ui);
 		return NULL;
 	}
 	UI_free(ui);
@@ -75,12 +91,12 @@ int pkcs11_finish(ENGINE * engine)
 
 int pkcs11_init(ENGINE * engine)
 {
-	if (!quiet)
+	if (verbose) {
 		fprintf(stderr, "initializing engine\n");
-
+	}
 	ctx = PKCS11_CTX_new();
 	if (PKCS11_CTX_load(ctx, module) < 0) {
-		fprintf(stderr, "unable to load module\n");
+		fprintf(stderr, "unable to load module %s\n", module);
 		return 0;
 	}
 	return 1;
@@ -90,6 +106,9 @@ int pkcs11_rsa_finish(RSA * rsa)
 {
 	if (pin) {
 		free(pin);
+	}
+	if (!default_module && module) {
+		free(module);
 	}
 	/* need to free RSA_ex_data? */
 	return 1;
@@ -120,7 +139,7 @@ static int hex_to_bin(const char *in, unsigned char *out, size_t * outlen)
 			else if ('A' <= c && c <= 'F')
 				c = c - 'A' + 10;
 			else {
-				printf("hex_to_bin(): invalid char '%c' in hex string\n", c);
+				fprintf(stderr,"hex_to_bin(): invalid char '%c' in hex string\n", c);
 				*outlen = 0;
 				return 0;
 			}
@@ -129,7 +148,7 @@ static int hex_to_bin(const char *in, unsigned char *out, size_t * outlen)
 		if (*in == ':')
 			in++;
 		if (left <= 0) {
-			printf("hex_to_bin(): hex string too long");
+			fprintf(stderr,"hex_to_bin(): hex string too long");
 			*outlen = 0;
 			return 0;
 		}
@@ -160,9 +179,9 @@ EVP_PKEY *pkcs11_load_key(ENGINE * e, const char *s_slot_key_id,
 	char flags[64];
 	int logged_in = 0;
 
-	/* Parse s_slot_key_id: [slot:<slotNr>][;][id:<keyID>] or NULL,
+	/* Parse s_slot_key_id: [slot_<slotNr>][-][id_<keyID>] or NULL,
 	   with slotNr in decimal (0 = first slot, ...), and keyID in hex.
-	   E.g. "slot=1" or "id=46" or "slot=1;id=46 */
+	   E.g. "slot_1" or "id_46" or "slot_1-id_46 */
 	while (s_slot_key_id != NULL && *s_slot_key_id != '\0') {
 		char *p_sep1, *p_sep2;
 		char val[MAX_VALUE_LEN];
@@ -170,9 +189,9 @@ EVP_PKEY *pkcs11_load_key(ENGINE * e, const char *s_slot_key_id,
 
 		p_sep1 = strchr(s_slot_key_id, '_');
 		if (p_sep1 == NULL) {
-			printf("No \'_\' found in \"-key\" option \"%s\"\n", s_slot_key_id);
-			printf("Format: [slot_<slotNr>][-][id_<keyID>]\n");
-			printf("  with slotNr = 0, 1, ... and keyID = a hex string\n");
+			fprintf(stderr,"No \'_\' found in \"-key\" option \"%s\"\n", s_slot_key_id);
+			fprintf(stderr,"Format: [slot_<slotNr>][-][id_<keyID>]\n");
+			fprintf(stderr,"  with slotNr = 0, 1, ... and keyID = a hex string\n");
 			return NULL;
 		}
 
@@ -180,7 +199,7 @@ EVP_PKEY *pkcs11_load_key(ENGINE * e, const char *s_slot_key_id,
 		if (p_sep2 == NULL)
 			p_sep2 = p_sep1 + strlen(p_sep1);
 
-		/* val = the string between the = and the ; (or '\0') */
+		/* val = the string between the _ and the - (or '\0') */
 		val_len = p_sep2 - p_sep1 - 1;
 		if (val_len >= MAX_VALUE_LEN || val_len == 0)
 			fail("Too long or empty value after the \'-\' sign\n");
@@ -189,18 +208,18 @@ EVP_PKEY *pkcs11_load_key(ENGINE * e, const char *s_slot_key_id,
 
 		if (strncasecmp(s_slot_key_id, "slot", p_sep1 - s_slot_key_id) == 0) {
 			if (val_len >= 3) {
-				printf("Slot number \"%s\" should be a small integer\n", val);
+				fprintf(stderr,"Slot number \"%s\" should be a small integer\n", val);
 				return NULL;
 			}
 			slot_nr = atoi(val);
 			if (slot_nr == 0 && val[0] != '0') {
-				printf("Slot number \"%s\" should be an integer\n", val);
+				fprintf(stderr,"Slot number \"%s\" should be an integer\n", val);
 				return NULL;
 			}
 		} else if (strncasecmp(s_slot_key_id, "id", p_sep1 - s_slot_key_id)
 			   == 0) {
 			if (!hex_to_bin(val, key_id, &key_id_len)) {
-				printf("Key id \"%s\" should be a hex string\n", val);
+				fprintf(stderr,"Key id \"%s\" should be a hex string\n", val);
 				return NULL;
 			}
 			strcpy(buf, val);
@@ -208,7 +227,7 @@ EVP_PKEY *pkcs11_load_key(ENGINE * e, const char *s_slot_key_id,
 		} else {
 			memcpy(val, s_slot_key_id, p_sep1 - s_slot_key_id);
 			val[p_sep1 - s_slot_key_id] = '\0';
-			printf("Now allowed in -key: \"%s\"\n", val);
+			fprintf(stderr,"Now allowed in -key: \"%s\"\n", val);
 			return NULL;
 		}
 		s_slot_key_id = (*p_sep2 == '\0' ? p_sep2 : p_sep2 + 1);
@@ -217,7 +236,9 @@ EVP_PKEY *pkcs11_load_key(ENGINE * e, const char *s_slot_key_id,
 	if (PKCS11_enumerate_slots(ctx, &slot_list, &count) < 0)
 		fail("failed to enumerate slots\n");
 
-	printf("Found %u slot%s\n", count, (count <= 1) ? "" : "s");
+	if(verbose) {
+		fprintf(stderr,"Found %u slot%s\n", count, (count <= 1) ? "" : "s");
+	}
 	for (n = 0; n < count; n++) {
 		slot = slot_list + n;
 		flags[0] = '\0';
@@ -236,13 +257,16 @@ EVP_PKEY *pkcs11_load_key(ENGINE * e, const char *s_slot_key_id,
 		if ((m = strlen(flags)) != 0) {
 			flags[m - 2] = '\0';
 		}
-		printf("[%u] %-25.25s  %-16s", n, slot->description, flags);
-		if (slot->token) {
-			printf("  (%s)",
-			       slot->token->label[0] ?
-			       slot->token->label : "no label");
+		
+		if(verbose) {
+			fprintf(stderr,"[%u] %-25.25s  %-16s", n, slot->description, flags);
+			if (slot->token) {
+				fprintf(stderr,"  (%s)",
+				       slot->token->label[0] ?
+				       slot->token->label : "no label");
+			}
+			fprintf(stderr,"\n");
 		}
-		printf("\n");
 	}
 
 	if (slot_nr == -1) {
@@ -251,40 +275,48 @@ EVP_PKEY *pkcs11_load_key(ENGINE * e, const char *s_slot_key_id,
 	} else if (slot_nr >= 0 && slot_nr < count)
 		slot = slot_list + slot_nr;
 	else {
-		printf("Invalid slot number: %d\n", slot_nr);
+		fprintf(stderr,"Invalid slot number: %d\n", slot_nr);
 		return NULL;
 	}
 	tok = slot->token;
 
+	if (tok == NULL) {
+		fprintf(stderr,"Found empty token; \n");
+		return NULL;
+	}
 	if (!tok->initialized) {
-		printf("Found uninitialized token; \n");
+		fprintf(stderr,"Found uninitialized token; \n");
 		return NULL;
 	}
 
 	if (isPrivate && !tok->userPinSet && !tok->readOnly) {
-		printf("Found slot without user PIN\n");
+		fprintf(stderr,"Found slot without user PIN\n");
 		return NULL;
 	}
 
-	printf("Found slot:  %s\n", slot->description);
-	printf("Found token: %s\n", slot->token->label);
+	if(verbose) {
+		fprintf(stderr,"Found slot:  %s\n", slot->description);
+		fprintf(stderr,"Found token: %s\n", slot->token->label);
+	}
 
 	if (PKCS11_enumerate_certs(tok, &certs, &count))
 		fail("unable to enumerate certificates\n");
 
-	printf("Found %u certificate%s:\n", count, (count <= 1) ? "" : "s");
-	for (n = 0; n < count; n++) {
-		PKCS11_CERT *c = certs + n;
-		char *dn = NULL;
+	if(verbose) {
+		fprintf(stderr,"Found %u certificate%s:\n", count, (count <= 1) ? "" : "s");
+		for (n = 0; n < count; n++) {
+			PKCS11_CERT *c = certs + n;
+			char *dn = NULL;
 
-		printf("  %2u    %s", n + 1, c->label);
-		if (c->x509)
-			dn = X509_NAME_oneline(X509_get_subject_name(c->x509), NULL, 0);
-		if (dn) {
-			printf(" (%s)", dn);
-			OPENSSL_free(dn);
+			fprintf(stderr,"  %2u    %s", n + 1, c->label);
+			if (c->x509)
+				dn = X509_NAME_oneline(X509_get_subject_name(c->x509), NULL, 0);
+			if (dn) {
+				fprintf(stderr," (%s)", dn);
+				OPENSSL_free(dn);
+			}
+			fprintf(stderr,"\n");
 		}
-		printf("\n");
 	}
 
 	while (1) {
@@ -298,33 +330,43 @@ EVP_PKEY *pkcs11_load_key(ENGINE * e, const char *s_slot_key_id,
 			pin = (char *) malloc(12);
 			get_pin(ui_method, pin, 12);
 		}
-		if (PKCS11_login(slot, 0, pin))
+		if (PKCS11_login(slot, 0, pin)) {
+			if(pin != NULL) {
+				free(pin);
+				pin = NULL;
+			}
 			fail("Card login failed\n");
+		}
 		logged_in++;
 	}
 
 	if (count == 0) {
-		printf("No keys found.\n");
+		fprintf(stderr,"No keys found.\n");
 		return NULL;
 	}
 
-	printf("Found %u key%s:\n", count, (count <= 1) ? "" : "s");
+	if(verbose) {
+		fprintf(stderr,"Found %u key%s:\n", count, (count <= 1) ? "" : "s");
+	}
 	for (n = 0; n < count; n++) {
 		PKCS11_KEY *k = keys + n;
 
-		printf("  %2u %c%c %s\n", n + 1,
-		       k->isPrivate ? 'P' : ' ', k->needLogin ? 'L' : ' ', k->label);
-
+		if(verbose) {
+			fprintf(stderr,"  %2u %c%c %s\n", n + 1,
+			       k->isPrivate ? 'P' : ' ', k->needLogin ? 'L' : ' ', k->label);
+		}
 		if (key_id_len != 0 && k->id_len == key_id_len &&
 		    memcmp(k->id, key_id, key_id_len) == 0) {
-			printf("        ID = %s\n", s_key_id);
+			if(verbose) {
+				fprintf(stderr,"        ID = %s\n", s_key_id);
+			}
 			selected_key = k;
 		}
 	}
 
 	if (selected_key == NULL) {
 		if (s_key_id != NULL) {
-			printf("No key with ID \"%s\" found.\n", s_key_id);
+			fprintf(stderr,"No key with ID \"%s\" found.\n", s_key_id);
 			return NULL;
 		} else		/* Take the first key that was found */
 			selected_key = &keys[0];
